7f4e508cd851399c46b38fe496d7f7f8854592e1
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-08 23:15:07 +0600

    env created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
-const uri = "mongodb+srv://dbuser1:mKr2bUi0WVyE4oJe@cluster0.fhhp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.fhhp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
