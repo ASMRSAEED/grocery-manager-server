f0df343a2152d318dba2a645f0c9b6e455d2431f
ASMRSAEED <asmrsaeed@gmail.com>
2022-05-08 23:04:25 +0600

    Server Created

---
+++ index.js
@@ -0,0 +1,95 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const ObjectId = require("mongodb").ObjectId;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const port = process.env.PORT || 5000;
+
+// use middleware
+app.use(cors())
+app.use(express.json())
+
+const uri = "mongodb+srv://dbuser1:mKr2bUi0WVyE4oJe@cluster0.fhhp5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+
+  try {
+    await client.connect()
+    const vegetableCollection = client.db("groceryShop").collection("vegetable");
+
+    //..........................Create(C):(POST)...........................//
+
+    // Create (POST)
+    app.post("/vegetable", async (req, res) => {
+      const veg = req.body;
+      const result = await vegetableCollection.insertOne(veg);
+      res.json(result);
+
+    })
+
+    // ........................Read(R):(GET).................................//
+    // Read (GET)
+    app.get("/vegetable", async (req, res) => {
+      const query = {}
+      const cursor = vegetableCollection.find(query)
+      const vegetables = await cursor.toArray()
+      res.send(vegetables)
+    })
+
+    // Get Single package
+    app.get("/vegetable/:id", async (req, res) => {
+      const id = req.params;
+      console.log("getting specific place", id);
+      const query = { _id: ObjectId(id) };
+      const vegetable = await vegetableCollection.findOne(query);
+      res.send(vegetable);
+    });
+
+    //...........................Update(U):(PUT)........................//
+    // Update quantity
+    app.put("/vegetable/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedStock = req.body;
+      const filter = { _id: ObjectId(id) };
+      const options = { upsert: true };
+      const updateDocument = {
+        $set: updatedStock
+      };
+      const result = await vegetableCollection.updateOne(
+        filter,
+        updateDocument,
+        options
+      );
+      console.log("updating", id);
+      res.send(result);
+    });
+
+
+    //...................Delete(D):(DELETE)..........................//
+    // Delete an inventory
+    app.delete("/vegetable/:id", async (req, res) => {
+      const id = req.params;
+      const query = { _id: ObjectId(id) };
+      const result = await vegetableCollection.deleteOne(query);
+      res.send(result);
+    });
+
+  }
+  finally {
+    // await client.close()
+  }
+
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send("Grocery Manager Running")
+})
+
+app.listen(port, () => {
+  console.log("Listening on port", port);
+})
